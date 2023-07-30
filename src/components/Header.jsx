7773bce1ffe70029e3bd9063bf8bee229af99550
Metixes <metixes@gmail.com>
2023-07-30 22:36:04 +0300

    fixed locale

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,8 +10,6 @@ import '../assets/style/header/_header.scss'
 
 export default function Header() {
     const navigate = useNavigate()
-    const location = useLocation()
-    console.log(location.pathname)
     const currentLanguage = localStorage.getItem('i18nextLng')
     const getWeathers = useStore(state => state.getWeathers)
     const cardsIsEmpty = useStore(state => state.cardsIsEmpty)
@@ -33,11 +31,10 @@ export default function Header() {
 
     const changeLang = (lang) => {
         i18n.changeLanguage(lang)
-        navigate(`./Weather/${lang}/dfg`)
+        navigate(`Weather/${lang}`)
         getWeathers(lang)
         cardsIsEmpty(lang)
     }
-    console.log(location.pathname)
     return(
         <div className='header'>
             <div className='header_wrapper'>
