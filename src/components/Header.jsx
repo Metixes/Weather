cd3722b0d7915bb99bd63d2a04ad86827c2827e3
Metixes <metixes@gmail.com>
2023-07-30 22:17:02 +0300

    change build

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { useStore } from '../data/store'
 import { Select } from 'antd'
 import { GlobalOutlined } from '@ant-design/icons'
@@ -10,6 +10,8 @@ import '../assets/style/header/_header.scss'
 
 export default function Header() {
     const navigate = useNavigate()
+    const location = useLocation()
+    console.log(location.pathname)
     const currentLanguage = localStorage.getItem('i18nextLng')
     const getWeathers = useStore(state => state.getWeathers)
     const cardsIsEmpty = useStore(state => state.cardsIsEmpty)
@@ -31,11 +33,11 @@ export default function Header() {
 
     const changeLang = (lang) => {
         i18n.changeLanguage(lang)
-        navigate(`./Weather/${lang}`)
+        navigate(`./Weather/${lang}/dfg`)
         getWeathers(lang)
         cardsIsEmpty(lang)
     }
-
+    console.log(location.pathname)
     return(
         <div className='header'>
             <div className='header_wrapper'>
