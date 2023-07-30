653c518cc4ececf4dcbb4cc913df432309efffab
Metixes <metixes@gmail.com>
2023-07-30 18:52:07 +0300

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
-    <Route path='/metixes.github.io/Weather/' element={<App />}/>
+    <App />
   </Router>
 )
