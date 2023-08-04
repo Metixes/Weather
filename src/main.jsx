153e60a3fba2211c6155191f2c39362dbfc9f6eb
Metixes <metixes@gmail.com>
2023-08-05 00:44:54 +0300

    change url

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
     <Routes>
-      <Route path='/:lang' element={<App />}/>
+      <Route path='https://forecastwinds.vercel.app/' element={<App />}/>
     </Routes>
   </Router>
 )
