793970f76f905f9bca82c49d2be53418a6d8b486
Metixes <metixes@gmail.com>
2023-08-05 00:49:48 +0300

    change url

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import i18n from './i18next/i18next'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
     <Routes>
-      <Route path='https://forecastwinds.vercel.app/' element={<App />}/>
+      <Route path={`/:${i18n.language}`} element={<App />}/>
     </Routes>
   </Router>
 )
