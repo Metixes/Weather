38a908ec4d1bcd304a37a78ec8836d719c6c090b
Metixes <metixes@gmail.com>
2023-08-05 15:52:16 +0300

    fixed prettier

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-import { Button, AutoComplete  } from 'antd'
+import { useNavigate } from 'react-router-dom'
+import { Button, AutoComplete } from 'antd'
 import { useStore } from '../data/store'
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next'
 
 import i18n from '../i18next/i18next'
 import useDebounce from '../data/useDebounce'
@@ -11,80 +11,80 @@ import axios from 'axios'
 import styles from '../assets/style/main/main.module.scss'
 
 export default function Main() {
-    const { t } = useTranslation();
+  const { t } = useTranslation()
 
-    const navigate = useNavigate()
-    const getWeather = useStore(state => state.getWeather)
-    const getLatLonWeather = useStore(state => state.getLatLonWeather)
+  const navigate = useNavigate()
+  const getWeather = useStore(state => state.getWeather)
+  const getLatLonWeather = useStore(state => state.getLatLonWeather)
 
-    const [inputCityName, setInputCityName] = useState('')
-    const [citiesList, setCitiesList] = useState([])
+  const [inputCityName, setInputCityName] = useState('')
+  const [citiesList, setCitiesList] = useState([])
 
-    const debounceInputValue = useDebounce(inputCityName, 500)
+  const debounceInputValue = useDebounce(inputCityName, 500)
 
-    useEffect(() => {
-        handelShowCurrentWeather(debounceInputValue)
-    }, [debounceInputValue])
+  useEffect(() => {
+    handelShowCurrentWeather(debounceInputValue)
+  }, [debounceInputValue])
 
-    useEffect(() => {
-        getLatLonWeather(i18n.language)
+  useEffect(() => {
+    getLatLonWeather(i18n.language)
 
-        // if(!i18n.language){
-        //     navigate(`/en/`) 
-        // }else{
-        //     navigate(`/${i18n.language}/`)
-        // }
-    }, [])
+    // if(!i18n.language){
+    //     navigate(`/en/`)
+    // }else{
+    //     navigate(`/${i18n.language}/`)
+    // }
+  }, [])
 
-    const handelShowWeather = (event, cityName, lng) => {
-        if(event.code === 'Enter')
-        getWeather(cityName, lng)
+  const handelShowWeather = (event, cityName, lng) => {
+    if (event.code === 'Enter') getWeather(cityName, lng)
+  }
+
+  const handelShowCurrentWeather = async value => {
+    if (value === '') return
+
+    const options = {
+      method: 'GET',
+      baseURL: `https://api.api-ninjas.com/v1/city?`,
+      params: {
+        name: value,
+        limit: 5,
+      },
+      headers: {
+        'X-Api-Key': '49l1hoU5qWpNBF+GahICKg==8kcYVF5gRNsB2H57',
+      },
     }
-
-    const handelShowCurrentWeather = async (value) => {
-        if(value === '') 
-        return
-
-        const options = {
-            method: 'GET',
-            baseURL: `https://api.api-ninjas.com/v1/city?`,
-            params: {
-                name: value,
-                limit: 5
-            },
-            headers: {
-                'X-Api-Key': '49l1hoU5qWpNBF+GahICKg==8kcYVF5gRNsB2H57'
-            }
-        }
-        try{
-            const { data } = await axios.request(options)
-            if(data){
-                const arrOptions = []
-                data.forEach(el => arrOptions.push({ value: el.name, key: crypto.randomUUID() }))
-                return setCitiesList(arrOptions)
-            }
-        }catch{
-            console.log('error')
-        }
+    try {
+      const { data } = await axios.request(options)
+      if (data) {
+        const arrOptions = []
+        data.forEach(el => arrOptions.push({ value: el.name, key: crypto.randomUUID() }))
+        return setCitiesList(arrOptions)
+      }
+    } catch {
+      console.log('error')
     }
+  }
 
-    return(
-        <div className={styles.main}>
-            <div className={styles.main_search}>
-                <AutoComplete className={styles.main_search_input}
-                    options={citiesList}
-                    value={inputCityName}
-                    onSelect={(text) => getWeather(text, i18n.language)}
-                    onChange={setInputCityName}
-                    placeholder={t("search-placeholder")}
-                    onKeyDown={(e) => handelShowWeather(e, inputCityName, i18n.language)}
-                />
-                <Button 
-                    className={styles.main_search_btn}
-                    onClick={() => getWeather(inputCityName, i18n.language)}>
-                    {t("search-button")}
-                </Button>
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.main}>
+      <div className={styles.main_search}>
+        <AutoComplete
+          className={styles.main_search_input}
+          options={citiesList}
+          value={inputCityName}
+          onSelect={text => getWeather(text, i18n.language)}
+          onChange={setInputCityName}
+          placeholder={t('search-placeholder')}
+          onKeyDown={e => handelShowWeather(e, inputCityName, i18n.language)}
+        />
+        <Button
+          className={styles.main_search_btn}
+          onClick={() => getWeather(inputCityName, i18n.language)}
+        >
+          {t('search-button')}
+        </Button>
+      </div>
+    </div>
+  )
 }
--- src/data/store.js
+++ src/data/store.js
@@ -5,238 +5,245 @@ import instance from '../config'
 
 const API_KEY = '5fedcbd7c2c4e234f857f50c88de30ae'
 
-export const useStore = create()(persist((set, get) => ({
-    cards: {},
-    currentUnits: '°C',
+export const useStore = create()(
+  persist(
+    (set, get) => ({
+      cards: {},
+      currentUnits: '°C',
 
-    cardsIsEmpty: (obj) => {
+      cardsIsEmpty: obj => {
         return Object.keys(obj).length === 0
-    },
-    getLatLonWeather: (lng) => {
-        if(get().cardsIsEmpty(get().cards) && navigator.geolocation){
-                navigator.geolocation.getCurrentPosition(async (position) => {
-                    const latitude = position.coords.latitude;
-                    const longitude = position.coords.longitude;
-                    try {
-                        const res = await instance.get('forecast', {
-                            params: {
-                                lat: latitude,
-                                lon: longitude,
-                                units: 'metric',
-                                cnt: 40,
-                                appid: API_KEY,
-                                lang: lng,
-                            },
-                        });
-                        const data = res.data;
-                        if(data){
-                            const card = {
-                                tempUnit: '°C',
-                                disabledCelsius: true,
-                                disabledFahrenheit: false,
-                                cityName: data.city.name,
-                                countryCode: data.city.country,
-                                cityDate: dayjs(new Date).locale(lng).format(lng === 'uk' ? 'dd, MMMM YYYY, HH:mm':'ddd MMMM YYYY, HH:mm'),
-                                weatherIcon: data.list[0].weather[0].icon,
-                                weatherDescription: data.list[0].weather[0].description,
-                                weatherCurrentTemp: data.list[0].main.temp.toFixed(),
-                                weatherPressure: data.list[0].main.pressure,
-                                nextData: data.list,
-                                formattedNextData: get().getCurrentData(data.list),
-                                weatherFeelsLike: data.list[0].main.feels_like.toFixed(),
-                                weatherHumidity: data.list[0].main.humidity,
-                                weatherWindSpeed: data.list[0].wind.speed,
-                                cardId: data.city.id,
-                            }
-            
-                            set((state) => ({
-                                cards: { ...state.cards, [data.city.id]: card },
-                            }))
-                        }
-                    } catch {
-                        console.error('error');
-                    }
-                (error) => {
-                console.error('Error getting geolocation:', error.message);
+      },
+      getLatLonWeather: lng => {
+        if (get().cardsIsEmpty(get().cards) && navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(async position => {
+            const latitude = position.coords.latitude
+            const longitude = position.coords.longitude
+            try {
+              const res = await instance.get('forecast', {
+                params: {
+                  lat: latitude,
+                  lon: longitude,
+                  units: 'metric',
+                  cnt: 40,
+                  appid: API_KEY,
+                  lang: lng,
+                },
+              })
+              const data = res.data
+              if (data) {
+                const card = {
+                  tempUnit: '°C',
+                  disabledCelsius: true,
+                  disabledFahrenheit: false,
+                  cityName: data.city.name,
+                  countryCode: data.city.country,
+                  cityDate: dayjs(new Date())
+                    .locale(lng)
+                    .format(lng === 'uk' ? 'dd, MMMM YYYY, HH:mm' : 'ddd MMMM YYYY, HH:mm'),
+                  weatherIcon: data.list[0].weather[0].icon,
+                  weatherDescription: data.list[0].weather[0].description,
+                  weatherCurrentTemp: data.list[0].main.temp.toFixed(),
+                  weatherPressure: data.list[0].main.pressure,
+                  nextData: data.list,
+                  formattedNextData: get().getCurrentData(data.list),
+                  weatherFeelsLike: data.list[0].main.feels_like.toFixed(),
+                  weatherHumidity: data.list[0].main.humidity,
+                  weatherWindSpeed: data.list[0].wind.speed,
+                  cardId: data.city.id,
                 }
+
+                set(state => ({
+                  cards: { ...state.cards, [data.city.id]: card },
+                }))
+              }
+            } catch {
+              console.error('error')
             }
-            // }else {
-            //     console.error('Geolocation is not supported by this browser.');
-            // }
-            );
-            
+            error => {
+              console.error('Error getting geolocation:', error.message)
+            }
+          })
+        } else {
+          console.error('Geolocation is not supported by this browser.')
         }
-    },
-    getWeathers: async (lng) => {
+      },
+      getWeathers: async lng => {
         const cards = get().cards
         const citiesId = Object.keys(cards).join()
 
-        try{
-            const { data } = await instance.get('group', {
-                params:{
-                    id: citiesId,
-                    lang: lng,
-                    units: 'metric',
-                    cnt: 40,
-                    appid: import.meta.env.VITE_API_KEY
+        try {
+          const { data } = await instance.get('group', {
+            params: {
+              id: citiesId,
+              lang: lng,
+              units: 'metric',
+              cnt: 40,
+              appid: import.meta.env.VITE_API_KEY,
+            },
+          })
+          if (data) {
+            set(state => {
+              const updatedCards = { ...state.cards }
+              data.list.forEach(el => {
+                updatedCards[el.id] = {
+                  ...updatedCards[el.id],
+                  weatherDescription: el.weather[0].description,
+                  cityDate: dayjs(new Date())
+                    .locale(lng)
+                    .format(lng === 'uk' ? 'dd, MMMM YYYY, HH:mm' : 'ddd MMMM YYYY, HH:mm'),
+                  formattedNextData:
+                    lng === 'he'
+                      ? get().getCurrentData(updatedCards[el.id].nextData).reverse() // reversed formattedDana if locale language is "he" for charts
+                      : get().getCurrentData(updatedCards[el.id].nextData), // and give default formattedData if locale language is not "he" for charts
                 }
+              })
+              return { cards: updatedCards }
             })
-            if(data){
-                set((state) => {
-                    const updatedCards = { ...state.cards }
-                    data.list.forEach((el) => {
-                        updatedCards[el.id] = {
-                            ...updatedCards[el.id],
-                            weatherDescription: el.weather[0].description,
-                            cityDate: dayjs(new Date).locale(lng).format(lng === 'uk' ? 'dd, MMMM YYYY, HH:mm':'ddd MMMM YYYY, HH:mm'),
-                            formattedNextData: lng === 'he' 
-                            ? 
-                            get().getCurrentData(updatedCards[el.id].nextData).reverse() // reversed formattedDana if locale language is "he" for charts 
-                            : 
-                            get().getCurrentData(updatedCards[el.id].nextData) // and give default formattedData if locale language is not "he" for charts
-                        };
-                    });
-                    return { cards: updatedCards }
-                })
-            }
-        }catch{
-            console.error('error')
+          }
+        } catch {
+          console.error('error')
         }
-    },
-    getWeather: async (cityValue, lng) => {
-        if(Object.values(get().cards).find(el => el.cityName === cityValue))
-        return
+      },
+      getWeather: async (cityValue, lng) => {
+        if (Object.values(get().cards).find(el => el.cityName === cityValue)) return
 
-        try{
-            const res = await instance.get('forecast', {
-                params: {
-                    q: `${cityValue}`,
-                    units: 'metric',
-                    cnt: 40,
-                    appid: API_KEY,
-                    lang: lng
-                }
-            })
-            const date = res.data
-            if(date){
-                const card = {
-                    tempUnit: '°C',
-                    disabledCelsius: true,
-                    disabledFahrenheit: false,
-                    cityName: date.city.name,
-                    countryCode: date.city.country,
-                    cityDate: dayjs(new Date).locale(lng).format(lng === 'uk' ? 'dd MMMM YYYY, HH:mm':'ddd MMMM YYYY, HH:mm'),
-                    weatherIcon: date.list[0].weather[0].icon,
-                    weatherDescription: date.list[0].weather[0].description,
-                    weatherCurrentTemp: date.list[0].main.temp.toFixed(),
-                    weatherPressure: date.list[0].main.pressure,
-                    nextData: date.list,
-                    formattedNextData: get().getCurrentData(date.list),
-                    weatherFeelsLike: date.list[0].main.feels_like.toFixed(),
-                    weatherHumidity: date.list[0].main.humidity,
-                    weatherWindSpeed: date.list[0].wind.speed,
-                    cardId: date.city.id.toString(),
-                }
+        try {
+          const res = await instance.get('forecast', {
+            params: {
+              q: `${cityValue}`,
+              units: 'metric',
+              cnt: 40,
+              appid: API_KEY,
+              lang: lng,
+            },
+          })
+          const date = res.data
+          if (date) {
+            const card = {
+              tempUnit: '°C',
+              disabledCelsius: true,
+              disabledFahrenheit: false,
+              cityName: date.city.name,
+              countryCode: date.city.country,
+              cityDate: dayjs(new Date())
+                .locale(lng)
+                .format(lng === 'uk' ? 'dd MMMM YYYY, HH:mm' : 'ddd MMMM YYYY, HH:mm'),
+              weatherIcon: date.list[0].weather[0].icon,
+              weatherDescription: date.list[0].weather[0].description,
+              weatherCurrentTemp: date.list[0].main.temp.toFixed(),
+              weatherPressure: date.list[0].main.pressure,
+              nextData: date.list,
+              formattedNextData: get().getCurrentData(date.list),
+              weatherFeelsLike: date.list[0].main.feels_like.toFixed(),
+              weatherHumidity: date.list[0].main.humidity,
+              weatherWindSpeed: date.list[0].wind.speed,
+              cardId: date.city.id.toString(),
+            }
 
-                set((state) => ({
-                    cards: { [date.city.id.toString()]: card, ...state.cards, },
-                }))
-            }
-        } catch{
-            console.error(alert(`Such a city does not exist. Check that the input is correct`))
+            set(state => ({
+              cards: { [date.city.id.toString()]: card, ...state.cards },
+            }))
+          }
+        } catch {
+          console.error(alert(`Such a city does not exist. Check that the input is correct`))
         }
-    },
-    getCurrentData : (items) => {
-        if(items.length){
-            const result = []
-            const uniqueDates = [...new Set(items.map(item => dayjs(item.dt_txt).format('DD.MM')))]
-            
-            for (const currentDate of uniqueDates) {
-                const temps = items.filter(item => dayjs(item.dt_txt).format('DD.MM') === currentDate).map(item => Number(item.main.temp))
-                const averageTemp = temps.reduce((sum, temp) => sum + temp, 0) / temps.length
-                result.push({d: currentDate, temp: Number(averageTemp.toFixed())})
-            }
-            
-            return result
+      },
+      getCurrentData: items => {
+        if (items.length) {
+          const result = []
+          const uniqueDates = [...new Set(items.map(item => dayjs(item.dt_txt).format('DD.MM')))]
+
+          for (const currentDate of uniqueDates) {
+            const temps = items
+              .filter(item => dayjs(item.dt_txt).format('DD.MM') === currentDate)
+              .map(item => Number(item.main.temp))
+            const averageTemp = temps.reduce((sum, temp) => sum + temp, 0) / temps.length
+            result.push({ d: currentDate, temp: Number(averageTemp.toFixed()) })
+          }
+
+          return result
         }
-    },
-    selectedCelsius : (cardId) => {
-        const cards = get().cards;
-        const currentCard = cards[cardId];
+      },
+      selectedCelsius: cardId => {
+        const cards = get().cards
+        const currentCard = cards[cardId]
         const isCelsius = cards[cardId].disabledCelsius
         const isFahrenheit = cards[cardId].disabledFahrenheit
 
-        if(currentCard) {
-            function getCelsius(celsius) {
-                return Number(((celsius - 32) * 5/9).toFixed());
-            }
+        if (currentCard) {
+          function getCelsius(celsius) {
+            return Number((((celsius - 32) * 5) / 9).toFixed())
+          }
 
-            const tempInCelsius = currentCard.formattedNextData.map(celsius => {
-                return { d: celsius.d, temp: getCelsius(celsius.temp) };
-            });
-            
-            const currentTempInCelsius = getCelsius(currentCard.weatherCurrentTemp)
-            const feelsLikeTempInCelsius = getCelsius(currentCard.weatherFeelsLike)
+          const tempInCelsius = currentCard.formattedNextData.map(celsius => {
+            return { d: celsius.d, temp: getCelsius(celsius.temp) }
+          })
 
-            set((state) => ({
-                cards: {
-                    ...state.cards,
-                    [cardId]: {
-                        ...currentCard,
-                        disabledCelsius : !isCelsius,
-                        disabledFahrenheit: !isFahrenheit,
-                        tempUnit: ' °C',
-                        formattedNextData : tempInCelsius,
-                        weatherCurrentTemp: currentTempInCelsius,
-                        weatherFeelsLike: feelsLikeTempInCelsius
-                    },
-                }
-            }));
+          const currentTempInCelsius = getCelsius(currentCard.weatherCurrentTemp)
+          const feelsLikeTempInCelsius = getCelsius(currentCard.weatherFeelsLike)
+
+          set(state => ({
+            cards: {
+              ...state.cards,
+              [cardId]: {
+                ...currentCard,
+                disabledCelsius: !isCelsius,
+                disabledFahrenheit: !isFahrenheit,
+                tempUnit: ' °C',
+                formattedNextData: tempInCelsius,
+                weatherCurrentTemp: currentTempInCelsius,
+                weatherFeelsLike: feelsLikeTempInCelsius,
+              },
+            },
+          }))
         }
-    },
-    selectedFahrenheit : (cardId) => {
-        const cards = get().cards;
-        const currentCard = cards[cardId];
+      },
+      selectedFahrenheit: cardId => {
+        const cards = get().cards
+        const currentCard = cards[cardId]
         const isCelsius = cards[cardId].disabledCelsius
         const isFahrenheit = cards[cardId].disabledFahrenheit
 
-        if(currentCard) {
-            function getFahrenheit(fahrenheit) {
-                return Number(((fahrenheit * 9 / 5) + 32).toFixed());
-            }
+        if (currentCard) {
+          function getFahrenheit(fahrenheit) {
+            return Number(((fahrenheit * 9) / 5 + 32).toFixed())
+          }
 
-            const tempInFahrenheit = currentCard.formattedNextData.map(fahrenheit => {
-                return { d: fahrenheit.d, temp: getFahrenheit(fahrenheit.temp) };
-            });
-        
-            const currentTempInFahrenheit = getFahrenheit(currentCard.weatherCurrentTemp)
-            const feelsLikeTempInFahrenheit = getFahrenheit(currentCard.weatherFeelsLike)
+          const tempInFahrenheit = currentCard.formattedNextData.map(fahrenheit => {
+            return { d: fahrenheit.d, temp: getFahrenheit(fahrenheit.temp) }
+          })
 
-            set((state) => ({
-                cards: {
-                    ...state.cards,
-                    [cardId]: {
-                        ...currentCard,
-                        disabledCelsius : !isCelsius,
-                        disabledFahrenheit: !isFahrenheit,
-                        tempUnit: ' °F',
-                        formattedNextData : tempInFahrenheit,
-                        weatherCurrentTemp: currentTempInFahrenheit,
-                        weatherFeelsLike: feelsLikeTempInFahrenheit, 
-                    },
-                }
-            }));
+          const currentTempInFahrenheit = getFahrenheit(currentCard.weatherCurrentTemp)
+          const feelsLikeTempInFahrenheit = getFahrenheit(currentCard.weatherFeelsLike)
+
+          set(state => ({
+            cards: {
+              ...state.cards,
+              [cardId]: {
+                ...currentCard,
+                disabledCelsius: !isCelsius,
+                disabledFahrenheit: !isFahrenheit,
+                tempUnit: ' °F',
+                formattedNextData: tempInFahrenheit,
+                weatherCurrentTemp: currentTempInFahrenheit,
+                weatherFeelsLike: feelsLikeTempInFahrenheit,
+              },
+            },
+          }))
         }
-    },
-    deleteWeatherCard: (cardId) => {
-        set((state) => {
-            const cards = { ...state.cards };
-            delete cards[cardId];
-            return { cards };
-        });
+      },
+      deleteWeatherCard: cardId => {
+        set(state => {
+          const cards = { ...state.cards }
+          delete cards[cardId]
+          return { cards }
+        })
+      },
+    }),
+    {
+      name: 'weather-cards',
+      getStorage: () => localStorage,
     }
-}
-),{
-    name: 'weather-cards',
-    getStorage: () => localStorage
-}))
+  )
+)
